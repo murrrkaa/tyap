63ffa516ada13695c438c4010a1244e0a2f675d1
LuntikFromMoon <fantomblue25@gmail.com>
2026-03-21 16:13:51 +0300

    Added Ast folder and created variable and function declarations

---
+++ src/Ast/Declarations/AbstractVariableDeclaration.cs
@@ -0,0 +1,20 @@
+﻿namespace PsTiger.Ast.Declarations;
+
+/// <summary>
+/// Абстрактный класс с информацией о переменной или формальном параметре функции.
+/// </summary>
+public abstract class AbstractVariableDeclaration : Declaration
+{
+    protected AbstractVariableDeclaration(
+        string name, 
+        string typeName
+        )
+    {
+        Name = name;
+        TypeName = typeName;
+
+    }
+
+    public string Name { get; }
+    public string TypeName { get; } // int, float, string, bool
+}
---
+++ src/Ast/Declarations/Declaration.cs
@@ -0,0 +1,20 @@
+﻿using PsTiger.Ast.Attributes;
+
+using ValueType = PsTiger.Runtime.ValueType;
+
+namespace PsTiger.Ast.Declarations;
+
+public abstract class Declaration : AstNode
+{
+    private AstAttribute<ValueType> _resultType;
+
+    /// <summary>
+    /// Тип результата объявления.
+    /// </summary>
+    public ValueType ResultType
+    {
+        get => _resultType.Get();
+
+        set => _resultType.Set(value);
+    }
+}
