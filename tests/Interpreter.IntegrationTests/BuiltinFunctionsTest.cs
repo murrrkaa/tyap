f779a92317678d4454b107729245dc22f6a32bde
Marina Zalata <marinazalata20@yandex.ru>
2026-04-05 19:25:44 +0300

    InterpreterTest

---
+++ tests/Interpreter.IntegrationTests/BuiltinFunctionsTest.cs
@@ -0,0 +1,136 @@
+﻿using PsTiger.Interpreter;
+using PsTiger.Tests.TestLibrary.TestDoubles;
+using Xunit;
+
+namespace Interpreter.IntegrationTests;
+
+public class BuiltinFunctionsTest
+{
+    [Theory]
+    [MemberData(nameof(GetEvaluateBuiltinFunctionsData))]
+    public void Can_evaluate_builtin_functions(string code, string expectedOutput)
+    {
+        FakeEnvironment environment = new();
+        TigerInterpreter interpreter = new(environment);
+
+        interpreter.Execute(code);
+
+        Assert.Equal(expectedOutput, environment.BufferedOutput);
+    }
+
+    public static TheoryData<string, string> GetEvaluateBuiltinFunctionsData()
+    {
+        return new TheoryData<string, string>
+        {
+            // len
+            {
+                """
+                function main(): int {
+                    print(len("hello"));
+                    return 0;
+                }
+                """,
+                "5"
+            },
+
+            // substring
+            {
+                """
+                function main(): int {
+                    print(substring("hello", 1, 3));
+                    return 0;
+                }
+                """,
+                "ell"
+            },
+
+            // toString
+            {
+                """
+                function main(): int {
+                    print(toString(123));
+                    return 0;
+                }
+                """,
+                "123"
+            },
+
+            // parseInt
+            {
+                """
+                function main(): int {
+                    print(parseInt("42"));
+                    return 0;
+                }
+                """,
+                "42"
+            },
+
+            // toBool
+            {
+                """
+                function main(): int {
+                    print(toBool(0));
+                    return 0;
+                }
+                """,
+                "false"
+            },
+
+            {
+                """
+                function main(): int {
+                    print(toBool(5));
+                    return 0;
+                }
+                """,
+                "true"
+            },
+
+            // toFloat
+            {
+                """
+                function main(): int {
+                    print(toFloat(10));
+                    return 0;
+                }
+                """,
+                "10"
+            }
+        };
+    }
+
+    [Theory]
+    [MemberData(nameof(GetEvaluateInputFunctionsData))]
+    public void Can_evaluate_input_functions(
+        string code,
+        string input,
+        string expectedOutput)
+    {
+        FakeEnvironment environment = new();
+        environment.AddInput(input);
+
+        TigerInterpreter interpreter = new(environment);
+        interpreter.Execute(code);
+
+        Assert.Equal(expectedOutput, environment.BufferedOutput);
+    }
+
+    public static TheoryData<string, string, string> GetEvaluateInputFunctionsData()
+    {
+        return new TheoryData<string, string, string>
+        {
+            // readString
+            {
+                """
+                function main(): int {
+                    print(readString());
+                    return 0;
+                }
+                """,
+                "abc",
+                "abc"
+            }
+        };
+    }
+}
---
+++ tests/Interpreter.IntegrationTests/EntryPointTest.cs
@@ -0,0 +1,131 @@
+using PsTiger.Interpreter;
+using PsTiger.Tests.TestLibrary.TestDoubles;
+using Xunit;
+
+namespace Interpreter.IntegrationTests;
+
+public class EntryPointTest
+{
+    [Theory]
+    [MemberData(nameof(GetValidEntryPointData))]
+    public void Can_execute_main(
+        string code,
+        string expectedOutput,
+        int expectedExitCode)
+    {
+        FakeEnvironment environment = new();
+        TigerInterpreter interpreter = new(environment);
+
+        interpreter.Execute(code);
+
+        Assert.Equal(expectedOutput, environment.BufferedOutput);
+        Assert.Equal(expectedExitCode, interpreter.ExitCode);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetInvalidEntryPointData))]
+    public void Throws_on_invalid_entry_point(string code)
+    {
+        FakeEnvironment environment = new();
+        TigerInterpreter interpreter = new(environment);
+
+        Assert.ThrowsAny<Exception>(() => interpreter.Execute(code));
+    }
+
+    public static TheoryData<string, string, int> GetValidEntryPointData()
+    {
+        return new TheoryData<string, string, int>
+        {
+            // пуста€ программа
+            {
+                """
+                function main(): int {
+                    return 0;
+                }
+                """,
+                "",
+                0
+            },
+
+            // вывод
+            {
+                """
+                function main(): int {
+                    print("Hello");
+                    return 0;
+                }
+                """,
+                "Hello",
+                0
+            },
+
+            // возврат ненулевого exit code
+            {
+                """
+                function main(): int {
+                    return 5;
+                }
+                """,
+                "",
+                5
+            },
+
+            // выражение + вывод
+            {
+                """
+                function main(): int {
+                    print(1 + 2);
+                    return 0;
+                }
+                """,
+                "3",
+                0
+            }
+        };
+    }
+
+    public static TheoryData<string> GetInvalidEntryPointData()
+    {
+        return new TheoryData<string>
+        {
+            // нет main
+            """
+            function test(): int {
+                return 0;
+            }
+            """,
+
+            // main без return
+            """
+            function main(): int {
+                print(1);
+            }
+            """,
+
+            // неправильный тип return
+            """
+            function main(): int {
+                return "hello";
+            }
+            """,
+
+            // несколько функций (если у вас запрещено)
+            """
+            function main(): int {
+                return 0;
+            }
+
+            function other(): int {
+                return 0;
+            }
+            """,
+
+            // неверный тип main
+            """
+            function main(): string {
+                return "hello";
+            }
+            """
+        };
+    }
+}
