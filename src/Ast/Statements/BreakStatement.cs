14cc84ae3e6f5e2f2d2270c060e72508b7827181
LuntikFromMoon <fantomblue25@gmail.com>
2026-04-10 20:58:57 +0300

    Removed unnecessary files from Ast

--- src/Ast/Statements/BreakStatement.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PsTiger.Ast.Statements;
-
-/// <summary>
-/// Объявление инструкции break.
-/// </summary>
-public sealed class BreakStatement : Statement
-{
-    public override void Accept(IAstVisitor visitor) => visitor.Visit(this);
-}
--- src/Ast/Statements/ContinueStatement.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PsTiger.Ast.Statements;
-
-/// <summary>
-/// Объявление инструкции continue.
-/// </summary>
-public sealed class ContinueStatement : Statement
-{
-    public override void Accept(IAstVisitor visitor) => visitor.Visit(this);
-}
