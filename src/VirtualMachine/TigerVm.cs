5a894c783f5cd63eaf5f2456013a6b47adc07278
LuntikFromMoon <fantomblue25@gmail.com>
2026-04-10 22:48:56 +0300

    Changed VM

--- src/VirtualMachine/TigerVm.cs
+++ src/VirtualMachine/TigerVm.cs
@@ -1,6 +1,8 @@
 ﻿using PsTiger.Runtime;
 using PsTiger.VirtualMachine.Builtins;
 using PsTiger.VirtualMachine.Instructions;
+using System;
+using System.Collections.Generic;
 
 namespace PsTiger.VirtualMachine;
 
@@ -11,7 +13,6 @@ public class TigerVm
     private int _instructionPointer;
     private int _exitCode;
     private readonly Stack<Value> _evaluationStack;
-    private VariablesTable? _variables;
     private readonly Stack<ReturnContext> _returnStack;
     private Value _result;
 
@@ -24,7 +25,6 @@ public class TigerVm
         _instructionPointer = 0;
         _exitCode = 0;
         _evaluationStack = new Stack<Value>();
-        _variables = new VariablesTable();
         _returnStack = new Stack<ReturnContext>();
         _result = Value.Void;
     }
@@ -43,209 +43,11 @@ public class TigerVm
                     break;
 
                 case InstructionCode.Pop:
-                    _evaluationStack.Pop();
-                    break;
-
-                case InstructionCode.StoreVar:
-                    {
-                        Value value = _evaluationStack.Pop();
-                        string variableName = instruction.Operand.AsString();
-                        _variables!.AssignVariable(variableName, value);
-                    }
-
-                    break;
-
-                case InstructionCode.DefineVar:
-                    {
-                        Value value = _evaluationStack.Pop();
-                        string variableName = instruction.Operand.AsString();
-                        _variables!.DefineVariable(variableName, value);
-                    }
-
-                    break;
-
-                case InstructionCode.LoadVar:
-                    {
-                        string variableName = instruction.Operand.AsString();
-                        Value value = _variables!.GetVariable(variableName);
-                        _evaluationStack.Push(value);
-                    }
-
-                    break;
-
-                case InstructionCode.Add:
-                    {
-                        Value right = _evaluationStack.Pop();
-                        Value left = _evaluationStack.Pop();
-
-                        if (left.IsString() && right.IsString())
-                        {
-                            string result = left.AsString() + right.AsString();
-                            _evaluationStack.Push(new Value(result));
-                        }
-                        else if (left.IsInt() && right.IsInt())
-                        {
-                            _evaluationStack.Push(new Value(left.AsInt() + right.AsInt()));
-                        }
-                        else if (left.IsFloat() || right.IsFloat())
-                        {
-                            double l = left.IsFloat() ? left.AsFloat() : left.AsInt();
-                            double r = right.IsFloat() ? right.AsFloat() : right.AsInt();
-                            _evaluationStack.Push(new Value(l + r));
-                        }
-                        else
-                        {
-                            throw new InvalidOperationException(
-                                $"Cannot add values of types {left} and {right}"
-                            );
-                        }
-                    }
-
-                    break;
-
-                case InstructionCode.Subtract:
-                    {
-                        Value right = _evaluationStack.Pop();
-                        Value left = _evaluationStack.Pop();
-                        _evaluationStack.Push(new Value(left.AsInt() - right.AsInt()));
-                    }
-
-                    break;
-
-                case InstructionCode.Multiply:
-                    {
-                        Value right = _evaluationStack.Pop();
-                        Value left = _evaluationStack.Pop();
-                        _evaluationStack.Push(new Value(left.AsInt() * right.AsInt()));
-                    }
-
-                    break;
-
-                case InstructionCode.Divide:
-                    {
-                        Value right = _evaluationStack.Pop();
-                        Value left = _evaluationStack.Pop();
-                        _evaluationStack.Push(new Value(left.AsInt() / right.AsInt()));
-                    }
-
-                    break;
-
-                case InstructionCode.And:
-                    {
-                        Value right = _evaluationStack.Pop();
-                        Value left = _evaluationStack.Pop();
-                        _evaluationStack.Push(new Value((left.AsInt() != 0 && right.AsInt() != 0) ? 1 : 0));
-                    }
-
-                    break;
-
-                case InstructionCode.Or:
-                    {
-                        Value right = _evaluationStack.Pop();
-                        Value left = _evaluationStack.Pop();
-                        _evaluationStack.Push(new Value((left.AsInt() != 0 || right.AsInt() != 0) ? 1 : 0));
-                    }
-
-                    break;
-
-                case InstructionCode.Not:
-                    {
-                        Value operand = _evaluationStack.Pop();
-                        _evaluationStack.Push(new Value(operand.AsInt() == 0 ? 1 : 0));
-                    }
-
-                    break;
-
-                case InstructionCode.Equal:
-                    {
-                        Value right = _evaluationStack.Pop();
-                        Value left = _evaluationStack.Pop();
-                        _evaluationStack.Push(new Value(left.Equals(right) ? 1 : 0));
-                    }
-
-                    break;
-
-                case InstructionCode.NotEqual:
-                    {
-                        Value right = _evaluationStack.Pop();
-                        Value left = _evaluationStack.Pop();
-                        _evaluationStack.Push(new Value(left.Equals(right) ? 0 : 1));
-                    }
-
-                    break;
-
-                case InstructionCode.Less:
-                    {
-                        Value right = _evaluationStack.Pop();
-                        Value left = _evaluationStack.Pop();
-                        _evaluationStack.Push(new Value(left.LessThan(right) ? 1 : 0));
-                    }
-
-                    break;
-
-                case InstructionCode.LessOrEqual:
-                    {
-                        Value right = _evaluationStack.Pop();
-                        Value left = _evaluationStack.Pop();
-                        _evaluationStack.Push(new Value(left.LessThanOrEqual(right) ? 1 : 0));
-                    }
-
-                    break;
-
-                case InstructionCode.GreaterThan:
-                    {
-                        Value right = _evaluationStack.Pop();
-                        Value left = _evaluationStack.Pop();
-                        _evaluationStack.Push(new Value(left.GreaterThan(right) ? 1 : 0));
-                    }
-
-                    break;
-
-                case InstructionCode.GreaterThanOrEqual:
-                    {
-                        Value right = _evaluationStack.Pop();
-                        Value left = _evaluationStack.Pop();
-                        _evaluationStack.Push(new Value(left.GreaterThanOrEqual(right) ? 1 : 0));
-                    }
-
-                    break;
-
-                case InstructionCode.Jump:
-                    _instructionPointer = instruction.Operand.AsInt();
-                    break;
-
-                case InstructionCode.JumpIfTrue:
-                    {
-                        Value condition = _evaluationStack.Pop();
-                        if (condition.AsInt() != 0)
-                        {
-                            _instructionPointer = instruction.Operand.AsInt();
-                        }
-                    }
-
-                    break;
-
-                case InstructionCode.JumpIfFalse:
-                    {
-                        Value condition = _evaluationStack.Pop();
-                        if (condition.AsInt() == 0)
-                        {
-                            _instructionPointer = instruction.Operand.AsInt();
-                        }
-                    }
-
+                    if (_evaluationStack.Count > 0) _evaluationStack.Pop();
                     break;
 
                 case InstructionCode.CallBuiltin:
                     CallBuiltin((BuiltinFunctionCode)instruction.Operand.AsInt());
-                    break;
-
-                case InstructionCode.Call:
-                    {
-                        _returnStack.Push(new ReturnContext(_instructionPointer, _variables));
-                        _instructionPointer = instruction.Operand.AsInt();
-                    }
-
                     break;
 
                 case InstructionCode.Return:
@@ -259,142 +61,55 @@ public class TigerVm
                         if (_returnStack.Count == 0)
                         {
                             _result = returnValue;
-
-                            if (returnValue.IsInt())
-                            {
-                                _exitCode = returnValue.AsInt();
-                            }
-                            else if (returnValue.IsBool())
-                            {
-                                _exitCode = returnValue.AsBool() ? 1 : 0;
-                            }
-
+                            if (returnValue.IsInt()) _exitCode = returnValue.AsInt();
                             return _result;
                         }
                         else
                         {
-                            if (returnValue != Value.Void)
-                            {
-                                _evaluationStack.Push(returnValue);
-                            }
-
+                            if (returnValue != Value.Void) _evaluationStack.Push(returnValue);
                             ReturnContext context = _returnStack.Pop();
                             _instructionPointer = context.InstructionPointer;
-                            _variables = context.Variables;
                         }
                     }
-
-                    break;
-
-                case InstructionCode.StoreResult:
-                    _result = _evaluationStack.Pop();
                     break;
 
                 case InstructionCode.Halt:
                     if (_evaluationStack.Count > 0)
                     {
-                        _exitCode = _evaluationStack.Pop().AsInt();
+                        var finalVal = _evaluationStack.Pop();
+                        if (finalVal.IsInt()) _exitCode = finalVal.AsInt();
                     }
-
                     return _result;
 
-                case InstructionCode.PushVars:
-                    {
-                        int variableTableDepth = instruction.Operand.AsInt();
-                        VariablesTable? parentTable = (variableTableDepth != 0)
-                            ? _variables!.GetAncestor(variableTableDepth)
-                            : null;
-                        _variables = new VariablesTable(parentTable);
-                    }
-
-                    break;
-
-                case InstructionCode.PopVars:
-                    _variables = _variables!.Parent;
-                    break;
-
                 default:
-                    throw new NotImplementedException($"Unsupported instruction code: {instruction.Code}");
+                    throw new NotImplementedException($"Instruction {instruction.Code} is disabled for Epic 1 (Points 1-3)");
             }
         }
     }
 
     private void CallBuiltin(BuiltinFunctionCode code)
     {
-        switch (code)
+        if (code == BuiltinFunctionCode.Print)
         {
-            case BuiltinFunctionCode.Print:
-                _builtinFunctions.Print(_evaluationStack.Pop());
-                break;
-
-            case BuiltinFunctionCode.ReadInt:
-                _evaluationStack.Push(_builtinFunctions.ReadInt());
-                break;
-
-            case BuiltinFunctionCode.ReadFloat:
-                _evaluationStack.Push(_builtinFunctions.ReadFloat());
-                break;
-
-            case BuiltinFunctionCode.ReadString:
-                _evaluationStack.Push(_builtinFunctions.ReadString());
-                break;
-
-            case BuiltinFunctionCode.Len:
-                _evaluationStack.Push(_builtinFunctions.Len(_evaluationStack.Pop()));
-                break;
-
-            case BuiltinFunctionCode.Substring:
-                {
-                    Value count = _evaluationStack.Pop();
-                    Value start = _evaluationStack.Pop();
-                    Value value = _evaluationStack.Pop();
-                    _evaluationStack.Push(_builtinFunctions.Substring(value, start, count));
-                }
-
-                break;
-
-            case BuiltinFunctionCode.ToString:
-                _evaluationStack.Push(_builtinFunctions.ToString(_evaluationStack.Pop()));
-                break;
-
-            case BuiltinFunctionCode.ParseInt:
-                _evaluationStack.Push(_builtinFunctions.ParseInt(_evaluationStack.Pop()));
-                break;
-
-            case BuiltinFunctionCode.ToBool:
-                _evaluationStack.Push(_builtinFunctions.ToBool(_evaluationStack.Pop()));
-                break;
-
-            case BuiltinFunctionCode.ToFloat:
-                _evaluationStack.Push(_builtinFunctions.ToFloat(_evaluationStack.Pop()));
-                break;
-
-            default:
-                throw new ArgumentException($"Unknown builtin function: {code}");
+            _builtinFunctions.Print(_evaluationStack.Pop());
+        }
+        else
+        {
+            throw new ArgumentException($"Builtin function {code} is not implemented in this iteration.");
         }
     }
 
     private static void ValidateInstructions(IReadOnlyList<Instruction> instructions)
     {
         if (instructions.Count == 0)
-        {
             throw new InvalidOperationException("Invalid empty VM program");
-        }
 
-        InstructionCode lastInstructionCode = instructions[^1].Code;
-        if (lastInstructionCode != InstructionCode.Halt
-            && lastInstructionCode != InstructionCode.Return
-            && lastInstructionCode != InstructionCode.Jump)
+        InstructionCode last = instructions[^1].Code;
+        if (last != InstructionCode.Halt && last != InstructionCode.Return)
         {
-            throw new InvalidOperationException(
-                $"Last instruction must be {InstructionCode.Halt}, " +
-                $"{InstructionCode.Return} or {InstructionCode.Jump}, got {lastInstructionCode}"
-            );
+            throw new InvalidOperationException("Program must end with Halt or Return");
         }
     }
 
-    private record struct ReturnContext(
-        int InstructionPointer,
-        VariablesTable? Variables
-    );
+    private record struct ReturnContext(int InstructionPointer);
 }
--- src/VirtualMachineCodegen/TigerVmCodegen.cs
+++ src/VirtualMachineCodegen/TigerVmCodegen.cs
@@ -12,45 +12,15 @@ namespace PsTiger.VirtualMachineCodegen;
 
 public class TigerVmCodegen : IAstVisitor
 {
-    private static readonly IReadOnlyDictionary<string, BuiltinFunctionCode> BuiltinFunctionsMap =
-        new Dictionary<string, BuiltinFunctionCode>
-        {
-            { "print", BuiltinFunctionCode.Print },
-            { "readInt", BuiltinFunctionCode.ReadInt },
-            { "readFloat", BuiltinFunctionCode.ReadFloat },
-            { "readString", BuiltinFunctionCode.ReadString },
-            { "len", BuiltinFunctionCode.Len },
-            { "substring", BuiltinFunctionCode.Substring },
-            { "toString", BuiltinFunctionCode.ToString },
-            { "parseInt", BuiltinFunctionCode.ParseInt },
-            { "toBool", BuiltinFunctionCode.ToBool },
-            { "toFloat", BuiltinFunctionCode.ToFloat },
-        };
-
     private readonly InstructionsBuilder _builder = new();
     private CodegenSymbolsTable? _symbolsTable;
-    private readonly Stack<BasicBlock> _loopEndBlocks = new();
 
     public List<Instruction> GenerateCode(Program program)
     {
         _symbolsTable = new CodegenSymbolsTable(null);
 
-        foreach (Declaration decl in program.TopLevelStatements)
-        {
-            if (decl is FunctionDeclaration func)
-            {
-                BasicBlock block = _builder.CreateBasicBlock();
-                _symbolsTable.AddFunctionEntry(func.Name, block);
-            }
-        }
-
         BasicBlock mainBlock = _builder.CreateBasicBlock();
         _symbolsTable.AddFunctionEntry(program.MainFunction.Name, mainBlock);
-
-        foreach (Declaration decl in program.TopLevelStatements)
-        {
-            decl.Accept(this);
-        }
 
         program.MainFunction.Accept(this);
 
@@ -63,151 +33,6 @@ public class TigerVmCodegen : IAstVisitor
     public void Visit(LiteralExpression e)
     {
         _builder.Append(new Instruction(InstructionCode.Push, e.Value));
-    }
-
-    public void Visit(VariableAccessExpression e)
-    {
-        _builder.Append(new Instruction(InstructionCode.LoadVar, e.Variable.Name));
-    }
-
-    public void Visit(BinaryOperationExpression e)
-    {
-        e.Left.Accept(this);
-        e.Right.Accept(this);
-
-        InstructionCode code = e.Operation switch
-        {
-            BinaryOperation.Add => InstructionCode.Add,
-            BinaryOperation.Subtract => InstructionCode.Subtract,
-            BinaryOperation.Multiply => InstructionCode.Multiply,
-            BinaryOperation.Divide => InstructionCode.Divide,
-            BinaryOperation.And => InstructionCode.And,
-            BinaryOperation.Or => InstructionCode.Or,
-            BinaryOperation.Equal => InstructionCode.Equal,
-            BinaryOperation.NotEqual => InstructionCode.NotEqual,
-            BinaryOperation.LessThan => InstructionCode.Less,
-            BinaryOperation.LessThanOrEqual => InstructionCode.LessOrEqual,
-            BinaryOperation.GreaterThan => InstructionCode.GreaterThan,
-            BinaryOperation.GreaterThanOrEqual => InstructionCode.GreaterThanOrEqual,
-            _ => throw new NotImplementedException($"Unsupported binary operation: {e.Operation}"),
-        };
-        _builder.Append(new Instruction(code));
-    }
-
-    public void Visit(UnaryNotExpression e)
-    {
-        e.Operand.Accept(this);
-        _builder.Append(new Instruction(InstructionCode.Not));
-    }
-
-    public void Visit(FunctionCallExpression e)
-    {
-        foreach (Expression arg in e.Arguments)
-        {
-            arg.Accept(this);
-        }
-
-        if (e.Function is BuiltinFunction builtin)
-        {
-            if (BuiltinFunctionsMap.TryGetValue(builtin.Name, out BuiltinFunctionCode code))
-            {
-                _builder.Append(new Instruction(InstructionCode.CallBuiltin, (int)code));
-            }
-            else
-            {
-                throw new NotImplementedException($"Unknown builtin: {builtin.Name}");
-            }
-        }
-        else if (e.Function is FunctionDeclaration func)
-        {
-            BasicBlock block = _symbolsTable!.GetFunctionEntry(func.Name);
-            _builder.AppendJump(InstructionCode.Call, block);
-        }
-    }
-
-    public void Visit(AssignmentStatement e)
-    {
-        e.Value.Accept(this);
-        _builder.Append(new Instruction(InstructionCode.StoreVar, e.VariableName));
-    }
-
-    public void Visit(IfStatement e)
-    {
-        BasicBlock elseBlock = _builder.CreateBasicBlock();
-        BasicBlock finalBlock = _builder.CreateBasicBlock();
-
-        e.Condition.Accept(this);
-        _builder.AppendJump(InstructionCode.JumpIfFalse, elseBlock);
-
-        e.ThenBranch.Accept(this);
-        _builder.AppendJump(InstructionCode.Jump, finalBlock);
-
-        _builder.InsertPoint = elseBlock;
-        if (e.ElseBranch != null)
-        {
-            e.ElseBranch.Accept(this);
-        }
-
-        _builder.AppendJump(InstructionCode.Jump, finalBlock);
-
-        _builder.InsertPoint = finalBlock;
-    }
-
-    public void Visit(WhileStatement e)
-    {
-        BasicBlock loopStart = _builder.CreateBasicBlock();
-        BasicBlock loopEnd = _builder.CreateBasicBlock();
-        _loopEndBlocks.Push(loopEnd);
-
-        _builder.AppendJump(InstructionCode.Jump, loopStart);
-        _builder.InsertPoint = loopStart;
-
-        e.Condition.Accept(this);
-        _builder.AppendJump(InstructionCode.JumpIfFalse, loopEnd);
-
-        e.Body.Accept(this);
-        _builder.AppendJump(InstructionCode.Jump, loopStart);
-
-        _loopEndBlocks.Pop();
-        _builder.InsertPoint = loopEnd;
-    }
-
-    public void Visit(ForStatement e)
-    {
-        BasicBlock loopStart = _builder.CreateBasicBlock();
-        BasicBlock loopEnd = _builder.CreateBasicBlock();
-        _loopEndBlocks.Push(loopEnd);
-
-        PushLexicalScope();
-
-        e.Init.Accept(this);
-
-        _builder.AppendJump(InstructionCode.Jump, loopStart);
-        _builder.InsertPoint = loopStart;
-
-        e.Condition.Accept(this);
-        _builder.AppendJump(InstructionCode.JumpIfFalse, loopEnd);
-
-        e.Body.Accept(this);
-
-        e.Step.Accept(this);
-        _builder.AppendJump(InstructionCode.Jump, loopStart);
-
-        _loopEndBlocks.Pop();
-        _builder.InsertPoint = loopEnd;
-        PopLexicalScope();
-    }
-
-    public void Visit(BreakStatement e)
-    {
-        BasicBlock loopEnd = _loopEndBlocks.Peek();
-        _builder.AppendJump(InstructionCode.Jump, loopEnd);
-    }
-
-    public void Visit(ContinueStatement e)
-    {
-        BasicBlock loopEnd = _loopEndBlocks.Peek();
-        _builder.AppendJump(InstructionCode.Jump, loopEnd);
     }
 
     public void Visit(ReturnStatement e)
@@ -229,12 +54,6 @@ public class TigerVmCodegen : IAstVisitor
         }
     }
 
-    public void Visit(FunctionCallStatement e)
-    {
-        e.Call.Accept(this);
-        _builder.Append(new Instruction(InstructionCode.Pop));
-    }
-
     public void Visit(BlockStatement e)
     {
         foreach (AstNode node in e.Nodes)
@@ -246,18 +65,6 @@ public class TigerVmCodegen : IAstVisitor
         }
     }
 
-    public void Visit(VariableDeclaration d)
-    {
-        d.InitialValue.Accept(this);
-        _builder.Append(new Instruction(InstructionCode.DefineVar, d.Name));
-    }
-
-    public void Visit(ConstantDeclaration d)
-    {
-        d.InitialValue.Accept(this);
-        _builder.Append(new Instruction(InstructionCode.DefineVar, d.Name));
-    }
-
     public void Visit(FunctionDeclaration d)
     {
         BasicBlock functionBlock = _symbolsTable!.GetFunctionEntry(d.Name);
@@ -266,16 +73,7 @@ public class TigerVmCodegen : IAstVisitor
 
         try
         {
-            PushLexicalScope();
-
-            foreach (ParameterDeclaration param in d.Parameters)
-            {
-                _builder.Append(new Instruction(InstructionCode.DefineVar, param.Name));
-            }
-
             d.Body.Accept(this);
-
-            PopLexicalScope();
         }
         finally
         {
@@ -283,37 +81,7 @@ public class TigerVmCodegen : IAstVisitor
         }
     }
 
-    public void Visit(ParameterDeclaration d)
-    {
-    }
-
     public void Visit(Program e)
     {
-    }
-
-    public void Visit(BuiltinFunction e)
-    {
-    }
-
-    public void Visit(BuiltinFunctionParameter e)
-    {
-    }
-
-    private bool IsJump(InstructionCode code)
-    {
-        return code is InstructionCode.Jump or InstructionCode.JumpIfTrue or InstructionCode.JumpIfFalse or InstructionCode.Call;
-    }
-
-    private void PushLexicalScope()
-    {
-        int parentDepth = _symbolsTable?.Depth ?? 0;
-        _symbolsTable = new CodegenSymbolsTable(_symbolsTable);
-        _builder.Append(new Instruction(InstructionCode.PushVars, parentDepth));
-    }
-
-    private void PopLexicalScope()
-    {
-        _builder.Append(new Instruction(InstructionCode.PopVars));
-        _symbolsTable = _symbolsTable!.Parent;
     }
 }
